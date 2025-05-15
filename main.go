18ca7cf47b09e67e5fdde7a6887ef02def2727bf
soldatov_a <alex.soldatov.2006@gmail.com>
2025-05-15 18:00:08 +0300

    swagger

--- main.go
+++ main.go
@@ -3,9 +3,30 @@ package main
 import (
 	"APIdinyPodkluchiny/DB"
 	"APIdinyPodkluchiny/api"
+	_ "APIdinyPodkluchiny/docs" // docs генерируются Swagger
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"log"
 )
+
+//	@title			MemesApp
+//	@version		0.2
+//	@description	Система пояснения сложных информационных сообщений юмористического характера.
+//	@termsOfService	http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+//	@host		localhost:8080
+//	@BasePath	/
+
+// @Failure 400 {object} models.ErrorResponse
+// @Failure 500 {object} models.ErrorResponse
 
 func main() {
 	db := DB.InitDB()
@@ -16,6 +37,8 @@ func main() {
 	router := gin.Default()
 	router.Use(api.CORSMiddleware())
 	router.Use(api.RateLimiter())
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	router.GET("/memes", func(c *gin.Context) { DB.GetMeme(c, db) })
 	router.GET("/memes/:id", func(c *gin.Context) { DB.GetMemeByID(c, db) })
