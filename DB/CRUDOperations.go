97a05a62a7b7fe6791433a8bb3cef0ed440012dc
Alfa4Solo <malashkinivan@yandex.ru>
2025-05-20 02:17:30 +0300

    CRUDtoTagsAndCategories

--- DB/CRUDOperations.go
+++ DB/CRUDOperations.go
@@ -129,3 +129,160 @@ func DeleteMeme(c *gin.Context, db *gorm.DB) {
 	}
 	c.JSON(http.StatusOK, gin.H{"message": "meme deleted"})
 }
+
+// CreateTag создает новый тег
+// @Summary Создать новый тег
+// @Description Добавляет новый тег в базу данных
+// @Tags Tags
+// @Accept json
+// @Produce json
+// @Param meme body models.Tag true "Данные нового тега"
+// @Success 201 {object} models.Tag
+// @Failure 400 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /tags [post]
+func CreateTag(c *gin.Context, db *gorm.DB) {
+	var NewTag models.Tag
+	if err := c.ShouldBindJSON(&NewTag); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := db.Create(&NewTag).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, NewTag)
+}
+
+// UpdateTag обновляет тег
+// @Summary Обновть тег
+// @Description Обновляет данные тега по ID
+// @Tags Tags
+// @Accept json
+// @Produce json
+// @Param id path int true "ID тега"
+// @Param updates body map[string]interface{} true "Обновляемые поля"
+// @Success 200 {object} map[string]string
+// @Failure 400 {object} map[string]string
+// @Failure 404 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /tags/{id} [patch]
+func UpdateTag(c *gin.Context, db *gorm.DB) {
+	id := c.Param("id")
+	var updatedData map[string]interface{}
+	if err := c.BindJSON(&updatedData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	result := db.Model(&models.Tag{}).Where("idtag = ? ", id).Updates(updatedData)
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		return
+	}
+	if result.RowsAffected == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Tag not found"})
+		return
+	}
+	c.JSON(http.StatusOK, updatedData)
+}
+
+// DeleteTag удаляет тег
+// @Summary Удалить тег
+// @Description Удаляет тег по указанному ID
+// @Tags Tags
+// @Accept json
+// @Produce json
+// @Param id path int true "ID тега"
+// @Success 200 {object} map[string]string
+// @Failure 404 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /tags/{id} [delete]
+func DeleteTag(c *gin.Context, db *gorm.DB) {
+	id := c.Param("id")
+	result := db.Where("idtag = ? ", id).Delete(&models.Tag{})
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		return
+	}
+	if result.RowsAffected == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Tag not found"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "tag deleted"})
+}
+
+// CreateCategory создает новую категорию мемов
+// @Summary Создать новую категорию мемов
+// @Description Добавляет новую категорию мемов в базу данных
+// @Tags Categories
+// @Accept json
+// @Produce json
+// @Param meme body models.Category true "Данные новой категории"
+// @Success 201 {object} models.Category
+// @Failure 400 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /categories [post]
+func CreateCategory(c *gin.Context, db *gorm.DB) {
+	var NewCategory models.Category
+	if err := c.ShouldBindJSON(&NewCategory); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := db.Create(&NewCategory).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, NewCategory)
+}
+
+// UpdateCategory обновляет категорию мемов
+// @Summary Обновить категорию мемов
+// @Description Обновляет категорию мемов по указанному ID
+// @Tags Categories
+// @Accept json
+// @Produce json
+// @Param id path int true "ID категории"
+// @Param updates body map[string]interface{} true "Обновляемые поля"
+// @Success 200 {object} map[string]string
+// @Failure 400 {object} map[string]string
+// @Failure 404 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /categories [patch]
+func UpdateCategory(c *gin.Context, db *gorm.DB) {
+	id := c.Param("id")
+	var updatedData map[string]interface{}
+	if err := c.BindJSON(&updatedData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := db.Where("idcategory = ? ", id).Updates(updatedData).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, updatedData)
+}
+
+// DeleteCategory удаляет категорию мемов
+// @Summary Удалить категорию мемов
+// @Description Удаляет категорию мемов по указанному ID
+// @Tags Categories
+// @Accept json
+// @Produce json
+// @Param id path int true "ID категории"
+// @Success 200 {object} map[string]string
+// @Failure 404 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /categories/{id} [delete]
+func DeleteCategory(c *gin.Context, db *gorm.DB) {
+	id := c.Param("id")
+	result := db.Where("idcategory = ? ", id).Delete(&models.Category{})
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		return
+	}
+	if result.RowsAffected == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Category not found"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "category deleted"})
+}
--- go.mod
+++ go.mod
@@ -21,6 +21,7 @@ require (
 	github.com/bytedance/sonic v1.13.2 // indirect
 	github.com/bytedance/sonic/loader v0.2.4 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.7 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.9 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
 	github.com/go-openapi/jsonpointer v0.21.1 // indirect
@@ -48,8 +49,12 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
+	github.com/urfave/cli/v2 v2.27.6 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 	golang.org/x/arch v0.17.0 // indirect
 	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/net v0.40.0 // indirect
@@ -60,4 +65,5 @@ require (
 	google.golang.org/protobuf v1.36.6 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	sigs.k8s.io/yaml v1.4.0 // indirect
 )
